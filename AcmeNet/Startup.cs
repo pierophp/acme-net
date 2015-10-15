60368ba6844c4f72ef8b438e97db556db5515ea8
Piero Giusti <pierophp@gmail.com>
2015-10-14 23:49:39 -0300

    First Commit

---
+++ AcmeNet/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AcmeNet.Startup))]
+namespace AcmeNet
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
